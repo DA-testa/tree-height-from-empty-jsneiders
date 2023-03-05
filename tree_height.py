3384ae4bd0304c83f8508408f22fa130524fb9cb
Janis Sneiders <jsneiders222@gmail.com>
2023-03-05 16:42:54 +0200

    IV

--- tree_height.py
+++ tree_height.py
@@ -23,8 +23,7 @@ def compute_height(n, parents):
 
     return np.max(depth)
 def main():
-    choice = input("Enter F or I: ")
-    choice = choice.upper()
+    choice = input("Enter F or I: ").strip()
     if (choice == "I" ):
         n = int(input("Count: "))
         parents = list(map(int, input("Nodes: ").split()))
@@ -35,7 +34,8 @@ def main():
             n = int(file.readline().strip())
             parents = list(map(int, file.readline().strip().split()))
             height = compute_height(n, parents)
-    else: height = "Error"
+    else:
+        return
 
     print(height)
 
